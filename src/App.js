e8c2d7dac72c406d072730630403fde6553c9ae4
Jón Gunnar Þórarinsson <jongunnar0210@gmail.com>
2021-04-14 00:12:56 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { Col, Container, Row } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Navbar */}
+      <div className="nav-bar">I'm a NavBar</div>
+
+      {/* Welcome div */}
+      <Container fluid className="welcome">
+        <Row className='align-items-center'>
+          <Col>
+            <h3>Welcome To Our Studio!</h3>
+            <h1>IT'S NICE TO MEET YOU</h1>
+            <button>TELL ME MORE</button>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* <Container fluid>
+        <div class="row align-items-center">
+          <div class="col">
+            One of three columns
+          </div>
+        </div>
+      </Container> */}
+
+      <Container fluid className='test'>
+        <Row className="align-items-center">
+          <Col>
+            One of three columns
+          </Col>
+        </Row>
+      </Container>
+
+
+      {/* Services */}
+      <div className="services">
+        <h2>SERVICES</h2>
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+        <span>E-Commerce</span>
+        <span>Responsive Design</span>
+        <span>Web Security</span>
+      </div>
     </div>
   );
 }
